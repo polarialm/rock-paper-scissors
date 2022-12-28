275ccb732f2bcdfe7f27acd8f4d7d37dc1529b34
Almeida <101214869+polarialm@users.noreply.github.com>
2022-12-28 17:40:11 +0000

    Make the game interactive using it's own inputs

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
+
+function reset() {
+    playerPts = 0
+    computerPts = 0
+}
+
 function getComputerChoice() 
 {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random()*3)];
 }
 
-function getPlayerChoice()
+function getPlayerChoice(element)
 {
-    let input = prompt("Pick rock , paper or scissors.");
-    input = input.toLowerCase();
-    if (input !== "rock" && input !== "paper" && input !== "scissors")
-    {
-        alert('Wrong choice.');
-    } else 
-    {
-        return input;
-    }
+    let input = element.getAttribute('data-sel');
+    return input;
+
 }
 
 function round(playerSelection, computerSelection)
@@ -39,11 +39,50 @@ function round(playerSelection, computerSelection)
 
 function game ()
 {
+    let i = 0
     let playerPts = 0;
     let computerPts = 0;
-    for (let i = 0; i < 5; i++)
+    const buttons = document.querySelector('#btns').querySelectorAll('button')
+    let res = document.querySelector('#result').querySelector('p')
+    let playerScore = document.querySelector('#player-num')
+    let computerScore = document.querySelector('#computer-num')
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let playerChoice = getPlayerChoice(button)
+            let computerChoice = getComputerChoice()
+            let result = round(playerChoice, computerChoice);
+            switch (result)
+            {
+                case -1 :
+                    res.textContent = "You lost this round! " + computerChoice + " beats " + playerChoice + "."
+                    computerPts++;
+                    break;
+                case 0 :
+                    res.textContent = "Tie in this round!"
+                    break;
+                case 1 :
+                    res.textContent = "You won this round!" + playerChoice + " beats " + computerChoice + "."
+                    playerPts++;
+                    break;
+            }
+            if (playerPts >= 5 || computerPts >= 5) {
+                if (playerPts > computerPts) {
+                    res.textContent = 'The player wins!'
+                } else if (playerPts < computerPts) {
+                    res.textContent = 'The computer wins!'
+                } else {
+                    res.textContent = 'Tie!'
+                }
+                playerPts = 0
+                computerPts = 0;
+            }
+            playerScore.textContent = playerPts
+            computerScore.textContent = computerPts
+        })
+    })
+    /*for (let i = 0; i < 5; i++)
     {
-        let playerChoice = getPlayerChoice();
+        let playerChoice = getPlayerChoice()
         let computerChoice = getComputerChoice();
       let result = round(playerChoice, computerChoice);
       switch (result)
@@ -60,7 +99,7 @@ function game ()
             playerPts++;
             break;
       }
-    }
+    }*/
     if (playerPts === computerPts)
     {
         return ("Tie! \nPlayer points = " + playerPts + ".\nComputer points = " + computerPts + ".");
@@ -75,5 +114,5 @@ function game ()
     }
 }
 
-console.log(game())
+game()
 
